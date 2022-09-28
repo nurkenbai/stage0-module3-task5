f45c2c99298847cf88ac4f9cdc286730102d81b9
NURKEN <86570552+nurkenbai@users.noreply.github.com>
2022-09-28 17:34:52 +0500

    Task #5. Operators 1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,7 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
-        System.out.println(toBeDivided - 5);
+    public static void main(String[] args) {
+        divide(19);
+    }
+
+    static void divide(int toBeDivided) {
+
+        System.out.println(toBeDivided / (float) 5);
     }
 }
